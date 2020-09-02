65a863177b7ce7a7c584210de5a66a922816c619
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-09-02 12:48:31 +0200

    First commit

---
+++ src/build/precache-manifest.26c7e5fedb7bfa163f203da913e9bd0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0138cf5233346ea793954fccc087b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23afda6076c889cf8daf",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "da99f8c4bdec6337deb0",
+    "url": "/static/js/2.c643f89b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c643f89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23afda6076c889cf8daf",
+    "url": "/static/js/main.82ca53ad.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
